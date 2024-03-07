0e872a7006ffa1fb2d2e2798f634e83ade0501c6
Kushla95 <das.kushal95@gmail.com>
2024-03-07 17:56:01 +0530

    Added backend for the Match Page_IPL

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TeamPage } from "./pages/TeamPage";
+import { MatchPage } from "./pages/MatchPage";
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Routes>
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          />
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/main/java/com/zkteco/ipldashboard/controller/TeamController.java
+++ src/main/java/com/zkteco/ipldashboard/controller/TeamController.java
@@ -1,18 +1,23 @@
 package com.zkteco.ipldashboard.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zkteco.ipldashboard.model.Match;
 import com.zkteco.ipldashboard.model.Team;
 import com.zkteco.ipldashboard.repository.MatchRepository;
 import com.zkteco.ipldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
@@ -36,6 +41,15 @@ public class TeamController {
         // Pageable pageable = PageRequest.of(0, 4);
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate,
+                endDate);
+
     }
 
     @PostMapping("/teams")
